5aa78e90ab4b77a9c3ee5363240a7d5c44e6c99b
wiseallie <wiseallie@gmail.com>
2016-01-28 08:58:41 +0200

    warn if json column does not exist and not throw errors

--- lib/has_json_attributes_on.rb
+++ lib/has_json_attributes_on.rb
@@ -25,7 +25,22 @@ module HasJsonAttributesOn
       end
     end
 
+    def table_and_data_column_exist?(data_column, _table_name = self.table_name)
+      # check if we have database connection and the table columns
+      begin
+        raise "database table: #{self.table_name} does not exist" unless  ActiveRecord::Base.connection.table_exists? _table_name.to_s
+        # try and access columns
+        raise "column: #{data_column} of database table #{self.table_name} does not exist"  unless columns.map(&:name).include?(data_column.to_s)
+        return true
+      rescue Exception => e
+        puts "HasJsonAttributesOn => Model: #{name}#has_json_attributes_on, #{e.message}, skipping..."
+        puts "\n\n\nPLEASE RUN ALL MIGRATIONS FIRST BEFORE RUNNING ANYTHING ELSE!!!\n\n\n"
+        return false
+      end
+    end
+
     def has_json_attributes_on(data_column = :data, accessors = {})
+      return unless table_and_data_column_exist?(data_column, self.table_name)
       validated_data_column, data_column_sql_type = validate_json_attributes_data_column!(data_column)
       validated_accessors = validate_dynamic_accessors!(accessors)
       build_json_attributes(validated_data_column, data_column_sql_type, validated_accessors)
@@ -35,36 +50,42 @@ module HasJsonAttributesOn
 
     def validate_json_attributes_data_column!(data_column)
       data_column = data_column.to_sym
-      unless columns.map(&:name).include?(data_column.to_s)
-        raise ArgumentError,  "Model: #{name}#has_json_attributes_on, data_column: #{data_column} does not exist as a column in database"
-      end
+      begin
+        unless columns.map(&:name).include?(data_column.to_s)
+          raise ArgumentError,  "HasJsonAttributesOn => Model: #{name}#has_json_attributes_on, data_column: #{data_column} does not exist as a column in database"
+        end
 
-      data_column_sql_type = columns.detect{|x| x.name.to_s == data_column.to_s}.sql_type.to_s
-      unless data_column_sql_type.in?(VALUE_TYPE_CLASSES.keys)
-        raise ArgumentError,  "Model: #{name}#has_json_attributes_on, data_column: #{data_column} is of sql type: #{data_column_sql_type}, supported types are:#{VALUE_TYPE_CLASSES.keys}"
+        data_column_sql_type = columns.detect{|x| x.name.to_s == data_column.to_s}.sql_type.to_s
+        unless data_column_sql_type.in?(VALUE_TYPE_CLASSES.keys)
+          raise ArgumentError,  "HasJsonAttributesOn => Model: #{name}#has_json_attributes_on, data_column: #{data_column} is of sql type: #{data_column_sql_type}, supported types are:#{VALUE_TYPE_CLASSES.keys}"
+        end
+      rescue Exception => e
+        puts e
+        return [data_column, 'json']
       end
       return [data_column, data_column_sql_type]
     end
 
 
     def validate_dynamic_accessors!(accessors)
-      raise ArgumentError, "Model: #{name}#has_json_attributes_on, accessors must be a hash" unless accessors.is_a?(Hash)
+      raise ArgumentError, "HasJsonAttributesOn => Model: #{name}#has_json_attributes_on, accessors must be a hash" unless accessors.is_a?(Hash)
       accessors = accessors.symbolize_keys
       accessors.each do |k,v|
         if columns.map(&:name).include?(k.to_s)
-          raise ArgumentError, "Model: #{name}#has_json_attributes_on, accessor key:#{k} has already been defined as the model column"
+          raise ArgumentError, "HasJsonAttributesOn => Model: #{name}#has_json_attributes_on, accessor key:#{k} has already been defined as the model column"
         end
 
         begin
           v.assert_valid_keys(:type, :validates, :default)
         rescue Exception => e
-          raise ArgumentError, "Model: #{name}#has_json_attributes_on, accessor key:#{k}, #{e.message}"
+          raise ArgumentError, "HasJsonAttributesOn => Model: #{name}#has_json_attributes_on, accessor key:#{k}, #{e.message}"
         end
       end
       return accessors
     end
 
     def build_json_attributes(data_column, data_column_sql_type, accessors)
+
       cattr_accessor :_json_attributes_on
       self._json_attributes_on ||= {}.symbolize_keys
       self._json_attributes_on[data_column] ||= {
@@ -74,7 +95,7 @@ module HasJsonAttributesOn
         validations: {},
         delegators: [],
         data_column_sql_type: data_column_sql_type,
-        value_type_class: VALUE_TYPE_CLASSES[data_column_sql_type.to_s].constantize,
+        value_type_class: VALUE_TYPE_CLASSES[data_column_sql_type.to_s],
         value_type_instance: nil
       }.symbolize_keys
 
